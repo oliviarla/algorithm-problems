77f173118079da5220c2d5be5782bf6d12b3f825
minkyeong <alsrud713@gmail.com>
2021-02-14 22:26:32 +0900

    string reverse

---
+++ leetcode/344 Reverse String.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def reverseString(self, s):
+        """
+        :type s: List[str]
+        :rtype: None Do not return anything, modify s in-place instead.
+        """
+        
+        for i in range(len(s)//2):
+            s[i], s[-i-1] = s[-i-1], s[i]
+
+        ######파이썬다운 방식######
+        s.reverse()
+        ##########################
