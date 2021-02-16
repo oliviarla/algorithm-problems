68bfd496c1af856ed9e8d2996fa23d0504be8b4a
minkyeong <alsrud713@gmail.com>
2021-02-16 23:46:18 +0900

    reorder log files

--- leetcode/937 Reorder Log Files.py
+++ leetcode/937 Reorder Log Files.py
@@ -4,3 +4,12 @@ class Solution(object):
         :type logs: List[str]
         :rtype: List[str]
         """
+        digits, letters = [], []
+        for log in logs:
+            if log.split()[1].isdigit():
+                digits.append(log)
+            else:
+                letters.append(log)
+        letters.sort(key=lambda x: (x.split()[1:], x.split()[0]))
+        
+        return letters + digits
