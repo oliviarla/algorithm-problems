bcd4fa5a6bb69deeed73f5c163f74e7981bb9185
minkyeong <alsrud713@gmail.com>
2021-02-22 23:54:32 +0900

    array

--- leetcode/1 Two Sum.py
+++ leetcode/1 Two Sum.py
@@ -9,3 +9,14 @@ class Solution(object):
             for j in range(len(nums)): #i를 제외한 인덱스에서 target-nums[i]가 존재하는지 확인
                 if nums[j]==target-nums[i] and i!=j:
                     return i, j
+
+
+        # dictionary를 사용한 풀이법
+        nums_map={}
+
+        for i, num in enumerate(nums):
+            nums_map[num] = i #dictionary에 숫자들을 저장
+
+        for i, num in enumerate(nums):
+            if target - num in nums_map and i!=nums_map[target-num]:
+                return nums.index(num), nums_map[target-num]
