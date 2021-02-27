b6fed05dbcdaf97c8a33072b511f7af65051ff9d
minkyeong <alsrud713@gmail.com>
2021-02-27 23:46:40 +0900

    array

---
+++ leetcode/238 Product of Array Except Self.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        output = []
+        for i in range(len(nums)):
+            output.append(nums[i])
+            print(result)
