b2cc3d55cc25ec9dcb00751035d008f8b3645e66
minkyeong <alsrud713@gmail.com>
2021-02-28 23:51:30 +0900

    array

--- leetcode/238 Product of Array Except Self.py
+++ leetcode/238 Product of Array Except Self.py
@@ -5,6 +5,17 @@ class Solution(object):
         :rtype: List[int]
         """
         output = []
-        for i in range(len(nums)):
-            output.append(nums[i])
-            print(result)
+
+        # 자신을 제외한 왼쪽의 값을 곱해 p에 저장한 후 output 값에 적용
+        p = 1
+        for i in range(0, len(nums)):
+            output.append(p)
+            p = p * nums[i]
+
+        # 자신을 제외한 오른쪽의 값을 점차 곱해 p에 저장한 후 output 값에 적용
+        p = 1
+        for k in range(len(nums) - 1, 0 - 1, -1):
+            output[k] = output[k] * p
+            p = p * nums[k]
+
+        return output
