0f544b42e2fd70063f6eddf8f688ffd761471c9b
minkyeong <alsrud713@gmail.com>
2021-03-02 00:07:16 +0900

    array

--- leetcode/121 Best Time to Buy and Self Stock.py
+++ leetcode/121 Best Time to Buy and Self Stock.py
@@ -6,9 +6,19 @@ class Solution(object):
         """
 
         max_pt, min_pt = 0, 0
+        result = []
 
-        for i in range(len(prices)):
+        for i in range(1, len(prices)):
             if prices[max_pt] < prices[i]:
                 max_pt = i
             elif prices[min_pt] > prices[i]:
                 min_pt = i
+                max_pt = i
+
+            if min_pt < max_pt:
+                result.append(prices[max_pt] - prices[min_pt])
+
+        if result:
+            return max(result)
+        else:
+            return 0
