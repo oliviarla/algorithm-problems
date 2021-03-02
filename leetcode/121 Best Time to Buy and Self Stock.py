4685c58b9afdee0f65d7283782b0825143eafe3b
minkyeong <alsrud713@gmail.com>
2021-03-03 00:19:52 +0900

    array

--- leetcode/121 Best Time to Buy and Self Stock.py
+++ leetcode/121 Best Time to Buy and Self Stock.py
@@ -5,6 +5,7 @@ class Solution(object):
         :rtype: int
         """
 
+        # 내가 작성한 코드
         max_pt, min_pt = 0, 0
         result = []
 
@@ -22,3 +23,14 @@ class Solution(object):
             return max(result)
         else:
             return 0
+
+        # 저점과 현재 값과의 차이 계산을 이용한 풀이법
+
+        profit = 0
+        min_price = sys.maxsize # 시스템이 설정할 수 있는 가장 큰 값을 min_price로 선언해둠
+
+        for price in prices:
+            min_price = min(min_price, price) # 낮은 값을 찾음
+            profit = max(profit, price - min_price) # '가장 작은값과 현재값의 차이' 와 기존 profit 값중 큰 값을 찾음
+
+        return profit
