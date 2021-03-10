93105ff307d6ce35fded49455fa38c5ef204b866
minkyeong <alsrud713@gmail.com>
2021-03-10 23:48:04 +0900

    linked list

--- leetcode/2 Add Two Numbers.py
+++ leetcode/2 Add Two Numbers.py
@@ -10,13 +10,18 @@ class Solution(object):
         :type l2: ListNode
         :rtype: ListNode
         """
+
+        # 여러 함수들로 구현한 풀이법
+
+        # a, b 리스트에 reverse된 연결리스트의 노드들 저장
         a = self.toList(self.reverse(l1))
+        b = self.toList(self.reverse(l2))
 
-    b = self.toList(self.reverse(l2))
+        # 리스트 내의 숫자들을 str형식에서 int로 바꾸어 결과 계산
+        resultStr = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
 
-    resultStr = int(''.join(str(e) for e in a)) + int(''.join(str(e) for e in b))
-
-    return self.toReversedLinkedList(str(resultStr))
+        # 결과를 다시 연결리스트로 변환
+        return self.toReversedLinkedList(str(resultStr))
 
     def reverse(self, head):
         node, prev = head, None
@@ -39,5 +44,27 @@ class Solution(object):
         for r in result:
             node = ListNode(r)
             node.next = prev
-            prev = node
+            prev = node  # prev를 사용해 이전 노드를 node.next에 연결되도록 함
         return node
+
+    # 전가산기를 응용한 풀이법
+
+    def addTwoNumbers(self, l1, l2):
+        root = head = ListNode(0)
+
+        carry = 0
+        while l1 or l2 or carry:
+            sum = 0
+            # 두 입력값의 합 계산
+            if l1:
+                sum += l1.val
+                l1 = l1.next
+            if l2:
+                sum += l2.val
+                l2 = l2.next
+
+            # 몫과 나머지 값 계산
+            carry, val = divmod(sum + carry, 10)
+            head.next = ListNode(val)
+            head = head.next
+        return root.next
